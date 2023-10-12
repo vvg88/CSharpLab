8998ab70373e51439b293cda23033770bb02042e
Vladimir Grishanin <vladimir_grishanin@outlook.com>
2023-10-12 21:43:38 +0300

    Records and ranges

---
+++ CSharpLab/CSharpLab/Extentions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLab
+{
+    internal static class Extentions
+    {
+        public static string EnumerableToString<T>(this IEnumerable<T> items) =>
+            $"{{ {string.Join(", ", items)} }}";
+    }
+}
---
+++ CSharpLab/CSharpLab/RangesTest.cs
@@ -0,0 +1,23 @@
+﻿namespace CSharpLab
+{
+    internal static class RangesTest
+    {
+        public static void Run()
+        {
+            var array = new[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine(array.EnumerableToString());
+
+            var arrayTmp = array[1..];
+            Console.WriteLine(arrayTmp.EnumerableToString());
+
+            arrayTmp = array[^2..];
+            Console.WriteLine(arrayTmp.EnumerableToString());
+
+            arrayTmp = array[..^2];
+            Console.WriteLine(arrayTmp.EnumerableToString());
+
+            arrayTmp = array[1..^2];
+            Console.WriteLine(arrayTmp.EnumerableToString());
+        }
+    }
+}
